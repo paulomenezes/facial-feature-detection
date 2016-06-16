68304284e2cb1253f46f3936be96a4af2a953b09
Paulo Menezes <paulo.hgmenezes@gmail.com>
2016-06-16 14:38:03 -0300

    Face detection

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <opencv2\opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
+	Mat image = imread("Lenna.png", CV_LOAD_IMAGE_GRAYSCALE);
+	
+	equalizeHist(image, image);
+	
+	CascadeClassifier faceCascade;
+	
+	faceCascade.load("haarcascade_frontalface_default.xml");
+	
+	vector<Rect> faces;
+	faceCascade.detectMultiScale(image, faces);
+	
+	Mat output;
+	cvtColor(image, output, CV_GRAY2BGR);
+	
+	for (int i = 0; i < faces.size(); ++i) {
+		rectangle(output, faces[i], Scalar(0, 0, 255));
+	}
+	
+	imshow("faces", output);
+	waitKey();
+	
 	return 0;
 }
